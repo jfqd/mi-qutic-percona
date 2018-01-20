fa17a969d87722955bd877082994432f8596779b
jfqd <jfqd@blun.org>
2018-01-20 22:03:25 +0100

    do not use /var/tmp

--- copy/var/zoneinit/includes/30-mysqlinit.sh
+++ copy/var/zoneinit/includes/30-mysqlinit.sh
@@ -1,5 +1,6 @@
 mv /opt/local/etc/my.cnf{,.bak}
-mv /var/tmp/my.cnf /opt/local/etc/my.cnf
+mv /var/zoneinit/tmp/my.cnf /opt/local/etc/my.cnf
+rm -rf /var/zoneinit/tmp
 
 if [ ! -d /var/mysql/mysql ]; then
   cd /var/mysql && mysql_install_db 1>/dev/null 2>&1 || true
