7ec65d45a38ed95cc9454049dea691e22edab9a8
jfqd <jfqd@blun.org>
2018-01-21 14:59:24 +0100

    fix munin-plugin config

--- copy/var/zoneinit/includes/31-mysql.sh
+++ copy/var/zoneinit/includes/31-mysql.sh
@@ -4,17 +4,16 @@ IP_INTERNAL=$(mdata-get sdc:nics | /usr/bin/json -ag ip -c 'this.nic_tag === "ad
 
 log "getting mysql_password"
 if [[ $(mdata-get mysql_pass &>/dev/null)$? -eq "0" ]]; then
-    MYSQL_PW=$(mdata-get mysql_pass 2>/dev/null);
+  MYSQL_PW=$(mdata-get mysql_pass 2>/dev/null);
 else
-    MYSQL_PW=$(od -An -N8 -x /dev/random | head -1 | tr -d ' ');
-    mdata-put mysql_pw ${MYSQL_PW}
+  MYSQL_PW=$(od -An -N8 -x /dev/random | head -1 | tr -d ' ');
 fi
 
 # Get mysql_server_id from metadata if exists
 log "getting mysql_server_id"
 if [[ $(mdata-get mysql_server_id &>/dev/null)$? -eq "0" ]]; then
-    MYSQL_SERVER_ID=$(mdata-get mysql_server_id 2>/dev/null);
-    gsed -i "/^server-id/s/server-id.*/server-id = ${MYSQL_SERVER_ID}/" /opt/local/etc/my.cnf
+  MYSQL_SERVER_ID=$(mdata-get mysql_server_id 2>/dev/null);
+  gsed -i "/^server-id/s/server-id.*/server-id = ${MYSQL_SERVER_ID}/" /opt/local/etc/my.cnf
 fi
 
 # Generate svccfg happy password for quickbackup-percona
@@ -23,9 +22,6 @@ log "getting qb_pw"
 QB_PW=${QB_PW:-$(mdata-get mysql_qb_pass 2>/dev/null)} || \
 QB_PW=$(od -An -N8 -x /dev/random | head -1 | sed 's/^[ \t]*//' | tr -d ' ');
 QB_US=qb-$(zonename | awk -F\- '{ print $1 }');
-
-mdata-put mysql_qb_pass "${QB_PW}"
-mdata-put mysql_qb_uid "${QB_US}"
 
 # Workaround for using DHCP so IP_INTERNAL or IP_EXTERNAL is empty
 if [[ -z "${IP_INTERNAL}" ]] || [[ -z "${IP_EXTERNAL}" ]]; then
@@ -124,3 +120,6 @@ user = root
 password = ${MYSQL_PW}
 EOF
 chmod 0400 /root/.my.cnf
+
+# fix munin-plugin config
+sed -i -e "s/env.mysqlpassword/env.mysqlpassword ${MYSQL_PW}/" /opt/local/etc/munin/plugin-conf.d/mysql
