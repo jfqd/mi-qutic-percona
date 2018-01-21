a281d88af9d21a37c7cb7242cb87255d56d56f2e
jfqd <jfqd@blun.org>
2018-01-21 18:10:28 +0100

    add stub ssl config

--- copy/var/zoneinit/includes/30-mysqlinit.sh
+++ copy/var/zoneinit/includes/30-mysqlinit.sh
@@ -5,3 +5,8 @@ rm -rf /var/zoneinit/tmp
 if [ ! -d /var/mysql/mysql ]; then
   cd /var/mysql && mysql_install_db 1>/dev/null 2>&1 || true
 fi
+
+# create folder for certs
+mkdir /var/mysql/certs
+chown mysql:mysql /var/mysql/certs
+chmod 0700 /var/mysql/certs
