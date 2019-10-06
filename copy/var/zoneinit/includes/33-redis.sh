1aa101ffe6739b0d65a616aeda89eab48fc2c75a
jfqd <jfqd@blun.org>
2019-10-06 22:11:36 +0200

    increase redis maxmemory; prevent usage of risky commands for security reasons

--- copy/var/zoneinit/includes/33-redis.sh
+++ copy/var/zoneinit/includes/33-redis.sh
@@ -1,8 +1,16 @@
 gsed -i \
-     -e "s/# maxmemory <bytes>/maxmemory 1gb/" \
+     -e "s/# maxmemory <bytes>/maxmemory 2gb/" \
      -e "s/# maxmemory-policy noeviction/# maxmemory-policy allkeys-lfu/" \
      -e "s/# unixsocket \/tmp\/redis.sock/unixsocket \/var\/tmp\/redis.sock/"
      /opt/local/etc/redis.conf
+
+cat >> /opt/local/etc/redis.conf << EOF
+# prevent usage of these commands for security reasons
+rename-command FLUSHALL ""
+rename-command FLUSHDB ""
+rename-command CONFIG ""
+rename-command SWAPDB ""
+EOF
 
 touch /var/log/redis/redis.log
 chown redis:redis /var/log/redis/redis.log
