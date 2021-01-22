4ff8b4300458b5aa5c345836e96c11438ec01dcf
Stefan Husch <jfqd@blun.org>
2021-01-22 13:37:17 +0100

    fix typo

--- copy/var/zoneinit/includes/35-spiped.sh
+++ copy/var/zoneinit/includes/35-spiped.sh
@@ -2,9 +2,9 @@ if mdata-get redis_master_host 1>/dev/null 2>&1; then
   REDIS_MASTER_HOST=`mdata-get redis_master_host`
   sed -i "s/-t 127.0.0.1:26379/-t $REDIS_MASTER_HOST:26379/g" /opt/local/lib/svc/manifest/spiped-redis-replication.xml
   svccfg import /opt/local/lib/svc/manifest/spiped-redis-replication.xml
-  sed -i "s/# slaveof <masterip> <masterport>/slaveof 127.0.0.1 16379/g" nano /opt/local/etc/redis.conf
+  sed -i "s/# slaveof <masterip> <masterport>/slaveof 127.0.0.1 16379/g" /opt/local/etc/redis.conf
 fi
-svcadm enable svc:/pkgsrc/redis:default
+svcadm restart svc:/pkgsrc/redis:default
 
 if mdata-get mysql_replication_host 1>/dev/null 2>&1; then
   MYSQL_MASTER_HOST=`mdata-get mysql_replication_host`
